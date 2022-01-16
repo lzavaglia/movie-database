72a05dc19fe497b4d69bae48e5f9146c8ed7b73a
chriszsolyomy <chris.zsolyomy@gmail.com>
2022-01-16 11:48:06 -0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import { getYear } from '../utilities/Date';
 function Footer() {
     return (
         <footer>
+            <div className='hLine'></div>
             <p>&copy; {getYear()} Chris Zsolyomy, Lily Zavaglia, Lucille Chesshire.</p>
         </footer>
     )
