f60a447d6c3d305d9a0a7884bebc005225db87eb
chriszsolyomy <chris.zsolyomy@gmail.com>
2022-01-17 22:18:10 -0800

    Mobile header styling looking very close.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,35 @@
-// import Nav from './Nav';
-import Hamburger from './Hamburger';
+import { useState } from 'react';
+import Nav from './Nav';
 import NavSort from './NavSort';
-
+import { Link } from 'react-router-dom';
 function Header() {
+
+    const [navOpen, setNavOpen] = useState(false);
+
+    const showHideNav = () => {
+        setNavOpen(!navOpen);
+    }
+
     return (
-        <header>
-            <Hamburger />
+        <header className={navOpen ? 'show' : undefined}>
+            <h1 className="filmtown"><Link to="/">Film Town</Link></h1>
+          
+            <button className="btn-main-nav" 
+                    onMouseDown={(e) => { e.preventDefault(); }}
+                    onClick={showHideNav}>
+                <span className="hamburger-icon">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </span>
+                <span className="sr-only">Menu</span>
+            </button>
+            <Nav handleShowHideNav={showHideNav} />
             <NavSort />
+
             <div className='hLine'></div>
         </header>
     );
-}
+};
 
 export default Header
