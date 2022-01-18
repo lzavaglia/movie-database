879472c4e5c259c7f55ef11ccad72376a271cdf5
lucillechesshire <lucillec98@gmail.com>
2022-01-18 10:53:09 -0800

    layout movie posters
    layout for mobile to desktop home page complete with hardcoded html

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,18 +1,23 @@
 import moviePoster from '../images/massImg.png';
 import '../styles/components/_moviePoster.scss';
+import heart from '../images/heart.svg';
 
 
 function MovieCard() {
     return (
-        <div>
+        <div className='info-image-container'>
         <div className='poster-container'>
             <img className='mass-poster' src={moviePoster} alt="mass movie poster"/>
+            </div>
+            <div className='title-icon-container'>
             <p className='movie-title'>Mass</p>
-            <span className='movie-info-container'>
+            <img class='heart' src={heart}></img>
+            </div>
+            <div className='movie-info-container'>
             <p className='movie-date'>Oct. 8, 2021</p>                   
-            <p className='movie-time'>1h 50m</p>
-            </span>
-        </div>
+            <p className='movie-rating'>PG</p>
+            </div>
+        
         
     )
         </div>
