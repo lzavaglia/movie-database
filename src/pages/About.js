56104e6e59c187a300b128e5470bba3dafccb3a4
l zavaglia <lily.zavaglia@gmail.com>
2022-01-24 21:13:51 -0800

    tweaked footer and about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,8 +10,10 @@ const AboutPage = () => {
     return (
         <main>
 		    <section>
-                <h2 className="about-us">About Us</h2>
-                <div className='hLineShort'></div>
+                <div className="about-us-cont">
+                    <h2 className="about-us">About Us</h2>
+                    <div className='hLineShort'></div>
+                </div>
                 <h2 className="about-welcome">Welcome to Film Town!</h2>
                 <p className="about-text">We are a movie database service that can give you information on all things movies! Find out what's popular right now and which movies have the best ratings. You can also check out what's playing in theatres now as well as what movies are yet to come. Don't forget to add a heart to your favourite flicks to add them to your favourites!
                 </p>                
